e30b1f1ece68dc84baf6320a63df07c64662b3ea
Patrick Desjardins <pdesjardins@netflix.com>
2022-03-11 21:14:06 -0800

    Adding gradient to fade out the number of set

--- src/pages/Workout/WorkoutExerciseSets.tsx
+++ src/pages/Workout/WorkoutExerciseSets.tsx
@@ -7,34 +7,38 @@ export interface WorkoutExerciseSetsProps {
   activeSet: number;
 }
 
-const FULL_WIDTH_SETS = 350; // Calculated from  data in WorkoutGo.module.css flex weight (auto) of 1024-250-450-margins
 const WIDTH = 50;
+const OFFSET = 40;
 function getLeft(activeIndex: number): number {
   // With 350 we can clearly see 4 sets, so if we are reaching
   //the set 4, we need to move left at least 1 set which is defined by the width constant
   if (activeIndex > 4) {
-    return -WIDTH * (activeIndex - 4);
+    return -WIDTH * (activeIndex - 4) - OFFSET;
   }
-  return 0;
+  return -OFFSET;
 }
 export const WorkoutExerciseSets = (props: WorkoutExerciseSetsProps) => {
   return (
-    <div class={styles.WorkoutExerciseSets}>
-      <div class={styles.Moveable} style={{ left: getLeft(props.activeSet) }}>
-        {props.workoutExercise.exerciseSets.map((set, i) => (
-          <div classList={{ [styles.oneset]: true, [styles.notactive]: i !== props.activeSet }}>
-            <SingleWeightThinSelector
-              defaultWeight={set.weight}
-              height={420}
-              width={WIDTH}
-              minimumWeight={CONSTANTS.MIN_WEIGHT}
-              maximumWeight={CONSTANTS.MAX_WEIGHT}
-              getCurrentWeight={(weight) => {}}
-              turnOffHandle={true}
-            />
-          </div>
-        ))}
+    <>
+      <div class={styles.curtainLeft} />
+      <div class={styles.WorkoutExerciseSets}>
+        <div class={styles.Moveable} style={{ left: `${getLeft(props.activeSet)}px` }}>
+          {props.workoutExercise.exerciseSets.map((set, i) => (
+            <div classList={{ [styles.oneset]: true, [styles.notactive]: i !== props.activeSet }}>
+              <SingleWeightThinSelector
+                defaultWeight={set.weight}
+                height={420}
+                width={WIDTH}
+                minimumWeight={CONSTANTS.MIN_WEIGHT}
+                maximumWeight={CONSTANTS.MAX_WEIGHT}
+                getCurrentWeight={(weight) => {}}
+                turnOffHandle={true}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div class={styles.curtainRight} />
+    </>
   );
 };

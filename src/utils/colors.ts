8ca8240f50a32e76881df1127f74ec01615fd793
Patrick Desjardins <pdesjardins@netflix.com>
2022-03-13 16:39:34 -0700

    Optimization after doing a performance capture. Was able to trim 1/3 of the draw function by memoizing the color building function

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -21,11 +21,22 @@ export const colorByRep = [
 /**
  * Convert a hex color to rgba.
  *
+ * This function is memoized. While simple, making it so reduce of 1/3 the drawing time.
+ *
  * @param hexColorValue: string that start with #
  */
-export function hex2rgba(hexColorValue: string, a: number): string {
-  const r = parseInt(hexColorValue.slice(1, 3), 16);
-  const g = parseInt(hexColorValue.slice(3, 5), 16);
-  const b = parseInt(hexColorValue.slice(5, 7), 16);
-  return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
+export const hex2rgba = ((): ((hexColorValue: string, a: number) => string) => {
+  const m = new Map();
+  const expensiveHex2rgba = (hexColorValue: string, a: number) => {
+    if (m.has(hexColorValue)) {
+      return m.get(hexColorValue);
+    }
+    const r = parseInt(hexColorValue.slice(1, 3), 16);
+    const g = parseInt(hexColorValue.slice(3, 5), 16);
+    const b = parseInt(hexColorValue.slice(5, 7), 16);
+    const result = `rgba(${r}, ${g}, ${b}, ${a})`;
+    m.set(hexColorValue, result);
+    return result;
+  };
+  return expensiveHex2rgba;
+})();
